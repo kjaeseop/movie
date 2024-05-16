7d5634982aea83fc4535aee129af2cf2db62bb61
kjaeseop <165218212+kjaeseop@users.noreply.github.com>
2024-05-16 23:14:01 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -4,33 +4,23 @@ from tmdbv3api import Movie, TMDb
 
 movie = Movie()
 tmdb = TMDb()
-tmdb.api_key = '5c480203497c4bc2eb8f627bf6e110da'
+tmdb.api_key = '5ad18f8eb8ecc4361611fb25886a1c9f'
 tmdb.language = 'ko-KR'
 
 def get_recommendations(title):
-    # 영화 제목을 통해서 전체 데이터 기준 그 영화의 index 값을 얻기
-    idx = movies[movies['title'] == title].index[0]
-
-    # 코사인 유사도 매트릭스 (cosine_sim) 에서 idx 에 해당하는 데이터를 (idx, 유사도) 형태로 얻기
-    sim_scores = list(enumerate(cosine_sim[idx]))
- 
-    # 코사인 유사도 기준으로 내림차순 정렬
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    
-    # 자기 자신을 제외한 10개의 추천 영화를 슬라이싱
-    sim_scores = sim_scores[1:11]
-    
-    # 추천 영화 목록 10개의 인덱스 정보 추출
-    movie_indices = [i[0] for i in sim_scores]
-    
+    idx = movies[movies['title'] == title].index[0] #제목을 통해 index 값 얻기
+    sim_scores = list(enumerate(cosine_sim[idx])) # cosine_sim에서 (idx, 유사도)형태로 얻기
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True) #내림차순 정렬
+    sim_scores = sim_scores[1:11] ## 자신을 제외한 10개의 추천 영화를 슬라이싱
+    movie_indices = [i[0] for i in sim_scores] # 추천 10개의 인덱스 정보 추출
     # 인덱스 정보를 통해 영화 제목 추출
     images = []
     titles = []
     for i in movie_indices:
         id = movies['id'].iloc[i]
         details = movie.details(id)
-        
         image_path = details['poster_path']
+        
         if image_path:
             image_path = 'https://image.tmdb.org/t/p/w500' + image_path
         else:
@@ -45,7 +35,7 @@ movies = pickle.load(open('movies.pickle', 'rb'))
 cosine_sim = pickle.load(open('cosine_sim.pickle', 'rb'))
 
 st.set_page_config(layout='wide')
-st.header('JSflix')
+st.header('JS_CINEMA')
 
 movie_list = movies['title'].values
 title = st.selectbox('Choose a movie you like', movie_list)
